201ff9317644ff10f9703b659ea87fd4db75cf67
Ilsur <yuzlikeyevi@gmail.com>
2022-08-14 22:44:36 -0500

    this is a test

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// alert("Js File is Connected!")
+
+
+// select an elemnt by its id
+const titleEl = document.getElementById("title");
+// console.log(titleEl);
+
+const titleEl2 = document.querySelector(".main-title");
+// console.log(titleEl2);
+
+const pEl = document.querySelector(".cool");
+// console.log(pEl);
+// const aEl = document.getElementsByName('a');
+// console.log(aEl);
+document.querySelector("a").setAttribute("href", "https://google.com");
